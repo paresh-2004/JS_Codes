2350741c8eb7c6c9e34bf804e8bc0f30ee958a95
paresh-2004 <prajapatparesh412@gmail.com>
2025-07-30 18:48:07 +0530

    new update

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("hello")
+// console.log("hello")
+const contact = "9352008779"
+var Name = "Paresh Prajapati"
+let city = "Jaipur"
+newcity = "Vadodara"
+
+let current_state;
+
+console.log(Name);
+console.table([Name,contact,city,newcity, current_state]);
